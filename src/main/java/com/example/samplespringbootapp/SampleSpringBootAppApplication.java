b058307adf34ee8f507835a0c01d5dcf2eba6bbc
shigeharukaichi <Shigeharu.Kaichi@jmas.co.jp>
2025-05-24 23:11:51 +0900

    Initial commit

---
+++ src/main/java/com/example/samplespringbootapp/SampleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplespringbootapp/SampleSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
